f2af3b8192077f6d2a8200d79adca2b4b2ad2439
Nicole <nicoleotieno18@gmail.com>
2019-11-30 14:03:05 +0300

    get time of the day

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ import ReactDOM from "react-dom";
 // import * as serviceWorker from "./serviceWorker";
 function App() {
   const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-  return <h1>It is currently about {date.getHours() % 12} o'clock!</h1>;
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 12) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  return <h1>Good {timeOfDay}!</h1>;
 }
 ReactDOM.render(<App />, document.getElementById("root")); //App is the root component
 
