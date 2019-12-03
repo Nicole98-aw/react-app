058e72e3e730954485caccfbf71b578a68b4a477
Nicole <nicoleotieno18@gmail.com>
2019-12-03 14:37:56 +0300

    map components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 //App component is the entry point into the entire application
 import React from "react";
 import Joke from "./Components/Joke.js";
+import jokesData from "./Components/JokesData";
 // import logo from "./logo.svg";
 // import "./App.css";
 
 function App() {
-  return (
-    <div className="contacts">
-      <Joke
-        question="What's the best thing about Switzerland?"
-        punchline="I don't know,but the flag is a big plus!"
-      />
-      <Joke
-        question="Did you hear about the Mathematician who's afraid of big numbers?"
-        punchline="He'll stop at nothing to avoid them"
-      />
-      <Joke
-        question="Heard about the new restaurant called Karma?"
-        punchline="There's no menu! You get what you deserve."
-      />
-      <Joke
-        question="Did you hear about the actor who fell through the floorboards?"
-        punchline="He was just going through a stage."
-      />
-      <Joke
-        question="Would you like to know how to code?"
-        punchline="Yes I would."
-      />
-    </div>
-  );
+  const jokeComponents = jokesData.map(joke => (
+    <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
+  ));
+
+  return <div>{jokeComponents}</div>;
 }
 
 export default App;
---
+++ src/Components/JokesData.js
@@ -0,0 +1,29 @@
+const jokesData = [
+    {
+        id: 1,
+        question="What's the best thing about Switzerland?",
+        punchline="I don't know,but the flag is a big plus!"
+    },
+    {
+        id: 2,
+        question="Did you hear about the Mathematician who's afraid of big numbers?",
+        punchline="He'll stop at nothing to avoid them"
+    },
+    {
+        id: 3,
+        question="Heard about the new restaurant called Karma?",
+        punchline="There's no menu! You get what you deserve."
+    },
+    {
+        id: 4,
+        question="Did you hear about the actor who fell through the floorboards?",
+        punchline="He was just going through a stage."
+    },
+    {
+        id: 5,
+        question="Would you like to know how to code?",
+        punchline="Yes I would."
+    }
+]
+
+export default jokesData;
