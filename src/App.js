9a4c924408a842c936a183a52e67c9e0eaf81f69
Nicole <nicoleotieno18@gmail.com>
2019-12-04 10:26:24 +0300

    modify TodoItem file

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@
 import React from "react";
 import Product from "./Components/Product";
 import productsData from "./Components/nschoolProduct";
+import TodoItem from "./Components/TodoItem";
+import todosData from "./Components/todoData";
 // import logo from "./logo.svg";
 // import "./App.css";
 
 function App() {
-  const productsComponents = productsData.map(product => (
-    <Product key={product.id} product={product} />
+  // const productsComponents = productsData.map(product => (
+  //   <Product key={product.id} product={product} />
+  // ));
+  // return <div>{productsComponents}</div>;
+
+  const todoItems = todosData.map(item => (
+    <TodoItem key={item.id} item={item} />
   ));
-  return <div>{productsComponents}</div>;
+
+  return <div>{todoItems}</div>;
 }
 
 export default App;
--- src/Components/nschoolProduct.js
+++ src/Components/nschoolProduct.js
@@ -1,34 +1,34 @@
-const productsData = [
-  {
-    id: 1,
-    name: "Pencil",
-    price: 12,
-    description: "Perfect for those who cannot remember things"
-  },
-  {
-    id: 2,
-    name: "Housing",
-    price: 20,
-    description: "Housing provided for students who can't commute"
-  },
-  {
-    id: 3,
-    name: "Coffee",
-    price: 32,
-    description: "Make up!"
-  },
-  {
-    id: 4,
-    name: "Make-up",
-    price: 12,
-    description: "Channell"
-  },
-  {
-    id: 5,
-    name: "Dresses",
-    price: 12,
-    description: "Luois Vouiton"
-  }
-];
+// const productsData = [
+//   {
+//     id: 1,
+//     name: "Pencil",
+//     price: 12,
+//     description: "Perfect for those who cannot remember things"
+//   },
+//   {
+//     id: 2,
+//     name: "Housing",
+//     price: 20,
+//     description: "Housing provided for students who can't commute"
+//   },
+//   {
+//     id: 3,
+//     name: "Coffee",
+//     price: 32,
+//     description: "Make up!"
+//   },
+//   {
+//     id: 4,
+//     name: "Make-up",
+//     price: 12,
+//     description: "Channell"
+//   },
+//   {
+//     id: 5,
+//     name: "Dresses",
+//     price: 12,
+//     description: "Luois Vouiton"
+//   }
+// ];
 
-export default productsData;
+// export default productsData;
