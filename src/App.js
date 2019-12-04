5b45648bcf91c015b3492f72e9b5b7b171583c16
Nicole <nicoleotieno18@gmail.com>
2019-12-04 14:58:06 +0300

    handle and bind click event

--- src/App.js
+++ src/App.js
@@ -7,33 +7,81 @@ import React from "react";
 // import logo from "./logo.svg";
 // import "./App.css";
 
-// function App() {
-// const productsComponents = productsData.map(product => (
-//   <Product key={product.id} product={product} />
-// ));
-// return <div>{productsComponents}</div>;
-//   const todoItems = todosData.map(item => (
-//     <TodoItem key={item.id} item={item} />
-//   ));
-//   return <div>{todoItems}</div>;
-// }
-
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      isLoggedIn: false
+      count: 0
     };
+
+    this.handleClick = this.handleClick.bind(this); // bind inside constructor
   }
+
+  handleClick() {
+    //handle events within class. Any event that uses setState has to be bound inside the constructor
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+
   render() {
-    let logIndicator;
-    if (this.state.isLoggedIn === true) {
-      logIndicator = "in";
-    } else {
-      logIndicator = "out";
-    }
-    return <h2>You are currently logged {logIndicator}</h2>;
+    return (
+      <div>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    );
   }
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       todos: todosData
+//     };
+//   }
+//   render() {
+// const todoItems = this.state.todos.map(item => (
+//   <TodoItem key={item.id} item={item} />
+// ));
+
+// return <div>{todoItems}</div>;
+//     return (
+//       <div>
+//         {this.state.todos.map(({ id, ...otherProps }) => (
+//           <TodoItem key={id} {...otherProps} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+// const productsComponents = productsData.map(product => (
+//   <Product key={product.id} product={product} />
+// ));
+// return <div>{productsComponents}</div>;
+
+/*--------------------------------------------------------*/
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isLoggedIn: false
+//     };
+//   }
+//   render() {
+//     let logIndicator;
+//     if (this.state.isLoggedIn === true) {
+//       logIndicator = "in";
+//     } else {
+//       logIndicator = "out";
+//     }
+//     return <h2>You are currently logged {logIndicator}</h2>;
+//   }
+// }
+
+/*-----------------------------------------------*/
 
 export default App;
--- src/Components/todoData.js
+++ src/Components/todoData.js
@@ -1,29 +1,29 @@
-// const todosData = [
-//   {
-//     id: 1,
-//     text: "Go to the gym",
-//     completed: false
-//   },
-//   {
-//     id: 2,
-//     text: "Grocery shopping",
-//     completed: true
-//   },
-//   {
-//     id: 3,
-//     text: "Visit Munich",
-//     completed: false
-//   },
-//   {
-//     id: 4,
-//     text: "Build a mansion",
-//     completed: true
-//   },
-//   {
-//     id: 5,
-//     text: "Learn salsa",
-//     completed: true
-//   }
-// ];
+const todosData = [
+  {
+    id: 1,
+    text: "Go to the gym",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Grocery shopping",
+    completed: true
+  },
+  {
+    id: 3,
+    text: "Visit Munich",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Build a mansion",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Learn salsa",
+    completed: true
+  }
+];
 
-// export default todosData;
+export default todosData;
